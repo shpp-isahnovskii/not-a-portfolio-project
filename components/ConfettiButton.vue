9e169a1f380da0dfd73cec521f765003b299db81
ISakhnovskyi <yaivan13@gmail.com>
2023-04-04 21:53:52 +0300

    physics prototype is ready

--- components/ConfettiButton.vue
+++ components/ConfettiButton.vue
@@ -2,7 +2,7 @@
   <div ref="target">
     <button
       class="celebrate-btn bg-emerald-400 hover:bg-emerald-300 text-white font-bold py-2 px-4 rounded-full tracking-wider shadow-lg shadow-emerald-400/50 hover:shadow-emerald-300/50 hover:scale-105 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]"
-      @click.prevent="animationHandler"
+      @click.prevent="confettiLaunch"
     >
       Celebrate now! 🎉
     </button>
@@ -10,72 +10,110 @@
 </template>
 
 <script setup lang="ts">
+const FPS = Math.floor(1000 / 60);
+
 let target = ref<null | HTMLElement>(null);
 let canvas: HTMLCanvasElement;
 let ctx: CanvasRenderingContext2D;
 
-let isLooping = ref(false);
+let isAction = ref(false);
+
+const cannon: any[] = [];
+let dx = 3;
+
+/* power of the launch */
+let minDy = 15;
+let maxDy = 17;
+
+let gravity = 0.5;
+let projectiles = 20;
+
+//rotation speed in rotationSpeeds
+const maxRS = 35;
+const minRS = 5;
 
 onMounted(() => {
-  addCanvas();
+  canvasInit();
+  confettiInit();
 });
 
-const addCanvas = () => {
+const canvasInit = () => {
   canvas = document.createElement('canvas');
   canvas.width = 600;
   canvas.height = 600;
   (target.value as HTMLElement).appendChild(canvas);
   (ctx as any) = canvas.getContext('2d');
+
+  ctx.textBaseline = 'middle';
+  ctx.textAlign = 'center';
+  ctx.font = '30px arial';
 };
 
-const animationHandler = () => {
-  isLooping.value = !isLooping.value;
+const confettiLaunch = () => {
+  isAction.value = !isAction.value;
+  frameTick();
+};
 
-  if (isLooping.value) {
-    let confetti = new Confetti(600 / 2 - 5, 500, 10, 'tomato');
-    confetti.update();
+const confettiInit = () => {
+  while (projectiles--) {
+    cannon.push({
+      name: '🎉',
+      x: canvas.width/2,
+      y: canvas.height/2,
+      dx: randomRange(-dx, dx),
+      dy: randomRange(-minDy, -maxDy),
+      rotationSpeed: randomRange(minRS, maxRS),
+    });
   }
 };
 
-class Confetti {
-  x: number;
-  y: number;
-  size: number;
-  color: string;
+const drawItem = (element: any) => {
+  ctx.translate(element.x, element.y);
+  ctx.rotate((element.rotationSpeed * Math.PI) / 180);
 
-  dy = -6;
-  dx = 0;
+  ctx.strokeText(element.name, 0, 0);
 
-  gravity = 0.1;
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+};
 
-  constructor(x: number, y: number, size: number, color: string) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.color = color;
+const frameTick = async () => {
+  if (isAction.value) {
+
+    drawScene();
+
+    setTimeout(() => {
+      window.requestAnimationFrame(frameTick);
+    }, FPS);
   }
-  update = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    this.draw();
+};
 
-    if (isLooping.value) window.requestAnimationFrame(this.update);
+/* the scene */
+const drawScene = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  cannon.forEach((item: Object, index: number) => {
+    drawItem(item);
+    cannon[index] = updateItemPhysics(item);
+  });
+};
 
-    this.dy += this.gravity;
-    this.y += this.dy;
-  };
+const updateItemPhysics = (item: any) => {
+  item.y = item.y + item.dy;
+  item.x = item.x + item.dx;
+  item.dy = item.dy + gravity;
+  item.rotationSpeed = item.rotationSpeed + 1;
+  return item;
+};
 
-  draw = () => {
-    (ctx as CanvasRenderingContext2D).beginPath();
-    ctx.moveTo(this.x, this.y);
-    //move line to: right -> bottom -> left -> top
-    ctx.lineTo(this.x + this.size, this.y);
-    ctx.lineTo(this.x + this.size, this.y + this.size);
-    ctx.lineTo(this.x, this.y + this.size);
-    ctx.lineTo(this.x, this.y);
-    ctx.closePath();
+const randomRange = (min: number, max: number) => {
+  return Math.random() * (max - min + 1) + min;
+};
 
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  };
-}
+
+
 </script>
+
+<style>
+canvas {
+  border: 1px solid blue;
+}
+</style>
